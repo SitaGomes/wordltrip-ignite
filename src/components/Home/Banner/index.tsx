ffe852ac524e9fba0d227db3b1ac0749d793d25f
SitaGomes <ansitagomes@gmail.com>
2021-12-18 17:32:43 +0100

    Add: Icon in home banner

--- src/components/Home/Banner/index.tsx
+++ src/components/Home/Banner/index.tsx
@@ -1,13 +1,47 @@
-import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import { Box, Flex, Heading, Image, Text, useBreakpointValue, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 export const HomeBanner: React.FC = () => {
-    return (
-      <Flex>
-        <Box bgImage="/background.png" bgPosition="center" p="8">
-          <Heading fontWeight="medium" fontSize={['xl', "3xl"]} color="gray.50">5 Continentes, infinitas possibilidades.</Heading>
-          <Text fontSize="sm" pt="2" color="gray.100">Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
-        </Box>
-      </Flex>
-    )
+
+  const isWideScreen = useBreakpointValue({base: false, lg: true})
+
+
+  return (
+    <Flex
+      bgImage="/assets/background.png"
+      bgPosition="center"
+      bgSize="cover"
+      bgRepeat="no-repeat"
+      justify="space-around"
+      p={["8", "8", "8", "0"]}
+    >
+      <VStack justify="center" align="start">
+
+        <VStack align="start" spacing="1">
+          <Heading
+            fontWeight="medium"
+            fontSize={['xl', "3xl"]}
+            color="gray.50"
+          >
+            5
+            Continentes,
+          </Heading>
+
+          <Heading
+            fontWeight="medium"
+            fontSize={['xl', "3xl"]}
+            color="gray.50"
+          >
+              infinitas
+              possibilidades.
+          </Heading>
+        </VStack>
+        
+        <Text fontSize={["sm", "md"]} pt="2" color="gray.100">Chegou a hora de tirar do papel a viagem que você sempre sonhou. </Text>
+      </VStack>
+
+      {!!isWideScreen && ( <Image  src="/assets/airplane.png"/> )}
+
+    </Flex>
+  )
 }
