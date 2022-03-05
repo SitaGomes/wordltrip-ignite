b442515bf0bf3edbb53cc4feb1cf2f3c3d3efd43
SitaGomes <ansitagomes@gmail.com>
2022-03-05 06:36:47 +0100

    Add: cities cards on mobile

--- src/pages/continents/[id].tsx
+++ src/pages/continents/[id].tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 import { GetServerSideProps, NextPage } from 'next/types'
 
 
-import { Box, HStack } from '@chakra-ui/react'
+import { Box, Text, HStack, Flex, Image } from '@chakra-ui/react'
 import { HeaderBar } from '../../components/Continents/Header'
 import { Banner } from '../../components/Continents/Banner'
 import { InfoBox } from '../../components/Continents/InfoBox'
@@ -11,10 +11,11 @@ import { Info } from '../../components/Continents/Info'
 
 interface ContinentPageProps {
   data: {
-    name: string;
+    country_name: string;
+    country_code: string;
     cities: [
       {
-        name: string,
+        city_name: string,
         country: string, 
         url: string
       }
@@ -33,14 +34,13 @@ interface ContinentPageProps {
 
 const ContinentPage: NextPage<ContinentPageProps> = ({data, id}) => {
 
-  console.log(data)
 
   return (
     <>
     
       <Head>
         <title>
-          {data.name} | WordlTrip
+          {data.country_name} | WordlTrip
         </title>
       </Head>
 
@@ -48,11 +48,11 @@ const ContinentPage: NextPage<ContinentPageProps> = ({data, id}) => {
         
         <HeaderBar />
 
-        <Banner id={id} countryName={data.name}/>
+        <Banner id={id} countryName={data.country_name}/>
 
         {/* Continent's content */}
         <Info exert={data.info?.exert}>
-          
+
           <HStack align={["flex-end", "center"]} justify="space-between" w={["100%", "400px"]}>
             <InfoBox information={data.info?.country_number}>países</InfoBox>
             <InfoBox information={data.info?.languagues}>línguas</InfoBox>
@@ -60,6 +60,29 @@ const ContinentPage: NextPage<ContinentPageProps> = ({data, id}) => {
           </HStack>
           
         </Info>
+
+        <Box>
+          <Text mb="6">Cidades +100</Text>
+
+          <Flex flexDirection={"column"} align="center" gap="4">
+            {data.cities.map((city) => (
+            
+              <Box key={city.city_name} w="250px">
+                <Image borderTopRadius="md" boxSize="250px" src={`/cities/${data.country_code}/${city.url}`} />
+                
+                <Box borderTopWidth="1" border="1px solid" borderColor="brand.500" background="white" p="4">
+                  <Flex flexDirection="column">
+                    <Text> {city.city_name} </Text>
+                    <Text color="gray.700"> {city.country} </Text>
+                  </Flex>
+                  <Image></Image>
+                </Box>
+              
+              </Box>
+            
+            ))}
+          </Flex>
+        </Box>
       
       </Box>
     </>
